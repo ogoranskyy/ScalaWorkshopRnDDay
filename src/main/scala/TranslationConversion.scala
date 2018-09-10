eb23ea018575cdc44bbfc2013802a070e9b9c5d0
Alex Goranskyy <o.goranskyy@epages.com>
2018-09-10 16:10:17 +0200

    explaining comment added

--- src/main/scala/TranslationConversion.scala
+++ src/main/scala/TranslationConversion.scala
@@ -1,3 +1,4 @@
 object TranslationConversion {
+   // compiler will try to apply this conversion, when String is found, but TranslatedString is expected
    implicit def stringToTranslatedString(s: String): TranslatedString = new TranslatedString(s)
 }
